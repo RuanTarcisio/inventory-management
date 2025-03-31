1342458aa3b088816c48967011c5ae1533eac0c7
Ruan Tarcisio <ruantarciisio@gmail.com>
2025-03-30 21:56:09 -0300

    sidebar semifull

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import React from "react";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
-import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/state";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
- const dispatch = useAppDispatch();
-  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
 
-  const toggleSidebar = () =>{
-    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
-  }
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
 
   return (
     <div className="flex justify-between items-center w-full mb-7">
@@ -43,12 +50,12 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
-              {/* {isDarkMode ? (
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
                 <Sun className="cursor-pointer text-gray-500" size={24} />
               ) : (
                 <Moon className="cursor-pointer text-gray-500" size={24} />
-              )} */}
+              )}
             </button>
           </div>
           <div className="relative">
@@ -59,14 +66,13 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            {/* <Image
+            <Image
               src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
               alt="Profile"
               width={50}
               height={50}
               className="rounded-full h-full object-cover"
-            /> */}
-            IMAGE
+            />
             <span className="font-semibold">Ed Roh</span>
           </div>
         </div>
